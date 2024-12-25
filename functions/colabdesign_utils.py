7ca1a9522fb1612e7a41ea9e921e27ef1e5355d6
Satish Gaurav <deepsatflow@gmail.com>
2024-12-26 01:02:55 +0530

    .

--- functions/colabdesign_utils.py
+++ functions/colabdesign_utils.py
@@ -126,7 +126,7 @@ def binder_hallucination(
     template_protein = prepare_inputs_for_loss("/root/bindcraft/target_A.pdb")
     custom_rmsd_loss(af_model, custom_inputs=template_protein)
     custom_fape_loss(af_model, custom_inputs=template_protein)
-    custom_dgram_loss(af_model, custom_inputs=template_protein)
+    custom_dgram_loss(af_model)
     # add the helicity loss
     # add_helix_loss(af_model, helicity_value)
 
@@ -700,38 +700,12 @@ def custom_fape_loss(self, custom_inputs, weight=1.0):
     self.opt["weights"]["custom_fape"] = weight
 
 
-def custom_dgram_loss(self, custom_inputs, weight=1.0):
-    """Calculate dgram loss for custom structure"""
-    tL, bL = self._target_len, self._binder_len
-
-    def dgram_loss(inputs, outputs, aatype=None):
-        batch = custom_inputs["batch"]
-        if aatype is None:
-            aatype = batch["aatype"]
-        pred = outputs["distogram"]["logits"]
-
-        x, weights = model.modules.pseudo_beta_fn(
-            aatype=aatype,
-            all_atom_positions=batch["all_atom_positions"],
-            all_atom_mask=batch["all_atom_mask"],
-        )
-        pred = pred[-bL:, -bL:, :]
-
-        dm = jnp.square(x[:, None] - x[None, :]).sum(-1, keepdims=True)
-        bin_edges = jnp.linspace(2.3125, 21.6875, pred.shape[-1] - 1)
-        true = jax.nn.one_hot((dm > jnp.square(bin_edges)).sum(-1), pred.shape[-1])
-
-        def loss_fn(t, p, m):
-            cce = -(t * jax.nn.log_softmax(p)).sum(-1)
-            return cce, (cce * m).sum((-1, -2)) / (m.sum((-1, -2)) + 1e-8)
-
-        weights = jnp.where(inputs["seq_mask"], weights, 0)
-        dgram_loss = _get_pw_loss(
-            true, pred, loss_fn, weights=weights, copies=1, return_mtx=False
-        )
+def custom_dgram_loss(self, weight=1.0):
+    def loss_fn(inputs, outputs):
+        dgram_loss = get_dgram_loss(inputs, outputs)
         return {"custom_dgram": dgram_loss}
 
-    self._callbacks["model"]["loss"].append(dgram_loss)
+    self._callbacks["model"]["loss"].append(loss_fn)
     self.opt["weights"]["custom_dgram"] = weight
 
 
