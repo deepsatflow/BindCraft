be6553e73981455571130cda168d74237ba108c4
Satish Gaurav <deepsatflow@gmail.com>
2024-12-25 16:31:24 +0530

    .

--- functions/colabdesign_utils.py
+++ functions/colabdesign_utils.py
@@ -675,23 +675,15 @@ def custom_fape_loss(self, custom_inputs, weight=1.0):
     def loss_fn(inputs, outputs):
 
         print("info about output structure: ", outputs.keys())
-        print(type(outputs["distogram"]))
-        print(type(outputs["experimentally_resolved"]))
-        print(type(outputs["masked_msa"]))
-        print(type(outputs["predicted_aligned_error"]))
-        print(type(outputs["predicted_lddt"]))
-        print(type(outputs["prev"]))
-        print(type(outputs["representations"]))
-        print(type(outputs["structure_module"]))
 
-        print(outputs["distogram"])
-        print(outputs["experimentally_resolved"])
-        print(outputs["masked_msa"])
-        print(outputs["predicted_aligned_error"])
-        print(outputs["predicted_lddt"])
-        print(outputs["prev"])
-        print(outputs["representations"])
-        print(outputs["structure_module"])
+        print(outputs["distogram"].keys())
+        print(outputs["experimentally_resolved"].keys())
+        print(outputs["masked_msa"].keys())
+        print(outputs["predicted_aligned_error"].keys())
+        print(outputs["predicted_lddt"].keys())
+        print(outputs["prev"].keys())
+        print(outputs["representations"].keys())
+        print(outputs["structure_module"].keys())
 
         # Get positions (219, 219, 3)
         positions = outputs["structure_module"]["final_atom_positions"]
